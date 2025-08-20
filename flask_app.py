bb657d346fac28e1035ece00a172ae0ed518acc6
Afsana721 <86984227+Afsana721@users.noreply.github.com>
2025-08-19 22:21:43 -0500

    Update port flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -19,5 +19,6 @@ class User(db.Model):
 def home():
     return "Hello, Flask app with PostgreSQL on Railway!"
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
