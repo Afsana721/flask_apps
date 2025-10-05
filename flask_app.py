5a2136bcabee07418ecc36070a3ab4cab5ef98d2
Afsana721 <86984227+Afsana721@users.noreply.github.com>
2025-10-05 00:16:56 -0500

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-from flask import request, redirect, url_for, flash , session,
+from flask import request, redirect, url_for, flash , session
 
 app = Flask(__name__, template_folder="templates")
 
