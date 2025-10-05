5d27515d4beaea508845d2d88ff015381fbf7213
Afsana721 <86984227+Afsana721@users.noreply.github.com>
2025-10-04 21:57:49 -0500

    added dashboard route on flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -74,6 +74,12 @@ def login():
         flash('Invalid login.')
     return render_template('business_Gem/login.html')
 
+#dashboard route
+@app.route('/dashboard')
+def dashboard():
+    return render_template('business_Gem/dashboard.html')
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
