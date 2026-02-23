a94b2a682f117c12181f0957e96f026eb30b7897
Afsana721 <86984227+Afsana721@users.noreply.github.com>
2026-02-23 13:36:30 -0600

    Add gallery route to render gallery.html

--- flask_app.py
+++ flask_app.py
@@ -301,5 +301,10 @@ def panoramic():
 def Valentine(): 
     return render_template('Valentine.html')
 
+#gallery html route 
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
